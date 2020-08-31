7299aa4f109d5f71f0d7f5a03eef2d8c2572ec56
GladarfinVS <gladarfinvs@gmail.com>
2020-08-31 14:54:07 +0500

    LinqBegin 16-31. Second part complete

--- LinqBegin1.cs
+++ LinqBegin1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Xml.Schema;
 
 namespace PT4Tasks
 {
@@ -108,9 +109,93 @@ namespace PT4Tasks
             #endregion
 
             #region Second group: LinqBegin16-LinqBegin31
+            //RU: В большинстве заданий из этого блока
+            //первое число должно быть КОЛИЧЕСТВОМ элементов выходной последовательности, 
+            //хотя в заданиях об этом не сказано
+            //EN: The first number (in the most tasks from this block) must be the Number of elements in output
 
+            //Task("LinqBegin16");
+            //var numbers = GetEnumerableInt().Where(e=>e>0);
+            //Put(numbers.Count(), numbers.Select(e => e));
 
+            //Task("LinqBegin17");
+            //var numbers = GetEnumerableInt().Where(e => e % 2 == 1).Distinct();
+            //Put(numbers.Count(), numbers);
 
+            //Task("LinqBegin18");
+            //var numbers = GetEnumerableInt().Where(e => e % 2 == 0 && e < 0).Reverse();
+            //Put(numbers.Count(), numbers);
+
+            //Task("LinqBegin19");
+            //var d = GetInt();
+            //var numbers = GetEnumerableInt().Where(e => e % 10 == d).Reverse().Distinct().Reverse();
+            //Put(numbers.Count(), numbers);
+
+            //Task("LinqBegin20");
+            //var numbers = GetEnumerableInt().Where(e => e >= 10 && e <= 99).OrderBy(e => e);
+            //Put(numbers.Count(), numbers);
+
+            //Task("LinqBegin21");
+            //var strings = GetEnumerableString().OrderBy(e => e.Length).ThenByDescending(e=>e);
+            //Put(strings.Count(), strings);
+
+            //Task("LinqBegin22");
+            //int k = GetInt();
+            //var a = GetEnumerableString().Where(e => e.Length == k && e[e.Length - 1] >= '0' && e[e.Length - 1] <= '9').OrderBy(s => s);
+            //Put(a.Count(), a);
+
+            //Task("LinqBegin23");
+            //int k = GetInt();
+            //var a = GetEnumerableInt().Skip(k - 1).Where(e => Math.Abs(e / 10) >= 1 && Math.Abs(e / 10) <= 9 && Math.Abs(e % 2) == 1).OrderByDescending(n=>n);
+            //Put(a.Count(), a);
+
+            //Task("LinqBegin24");
+            //int k = GetInt();
+            //var a = GetEnumerableString().Take(k-1).Where(e => e.Length % 2 == 1 && e[0]>='A' && e[0]<='Z').Reverse();
+            //Put(a.Count(), a);
+
+            //Task("LinqBegin25");
+            //int k1 = GetInt();
+            //int k2 = GetInt();
+            //Put(GetEnumerableInt().Skip(k1-1).Take(k2-k1+1).Where(e => e > 0).Sum());
+
+            //Task("LinqBegin26");
+            //int k1 = GetInt();
+            //int k2 = GetInt();
+            //var a = GetEnumerableString();
+            //Put(a.Take(k1-1).Union(a.Skip(k2).Take(a.Count()-k2)).Select(e=>e.Length).Average());
+
+            //Task("LinqBegin27");
+            //int d = GetInt();
+            //var a = GetEnumerableInt().SkipWhile(e => e < d).Where(n => n % 2 == 1 && n > 0).Reverse();
+            //Put(a.Count(), a);
+
+            //Task("LinqBegin28");
+            //int l = GetInt();
+            //var a = GetEnumerableString().TakeWhile(e => e.Length <= l).Where(c => c[c.Length - 1] >= 'A' && c[c.Length - 1] <= 'Z').OrderByDescending(s => s.Length).ThenBy(s => s);
+            //Put(a.Count(), a);
+
+            //Task("LinqBegin29");
+            //int d = GetInt();
+            //int k = GetInt();
+            //var a = GetEnumerableInt();
+            //var b = a.TakeWhile(e => e < d).Union(a.Skip(k - 1)).Distinct().OrderByDescending(e => e);
+            //Put(b.Count(), b);
+
+            //Task("LinqBegin30");
+            //int k = GetInt();
+            //var a = GetEnumerableInt();
+            //var b = a.Where(e => e % 2 == 0).Except(a.Skip(k)).Distinct().Reverse();
+            //Put(b.Count(), b);
+
+            //Task("LinqBegin31");
+            //int k = GetInt();
+            //var a = GetEnumerableString();
+            //var b = a.Take(k).Intersect(a.Reverse().
+            //        TakeWhile(c=> !char.IsDigit(c[c.Length-1])).Reverse())
+            //        .Distinct().OrderBy(s => s.Length).ThenBy(s=>s);
+            //Put(b.Count(), b);
+            
             #endregion
 
         }
